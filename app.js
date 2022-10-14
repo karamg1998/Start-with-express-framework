6f53da953715aae0ebac14a796a776af00f063ba
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-14 20:59:30 +0530

    task 3 problem 3

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use('/add-product',(req,res,next)=>{
-res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add</button></form>');
+res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="text" name="size"><br><button type="submit">add</button></form>');
 });
 
 app.use('/product',(req,res,next)=>{
-console.log(req.body.title);
+console.log('name of product is '+req.body.title);
+console.log('size of product is '+req.body.size);
 res.redirect('/');
 });
 
