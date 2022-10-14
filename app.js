9ab5edcfc770f5191c9741c325d3f8e05d24e653
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-14 20:49:37 +0530

    task 3 problem 1

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const http = require('http');
+
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-console.log('this is first middleware');
-next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product',(req,res,next)=>{
+res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log('this is second middleware');
+app.use('/product',(req,res,next)=>{
+console.log(req.body.title);
+res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello express.js</h1>');
     });
 
 app.listen(3000);
