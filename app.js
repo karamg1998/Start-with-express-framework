fd723ea7af97aeccd551e96819e465b831150d8e
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2022-10-16 12:14:42 +0530

    task 6 (1)

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express=require('express');
-
 const bodyParser=require('body-parser');
-
 const app=express();
-
 app.use(bodyParser.urlencoded({extended: false}));
-const adminRoutes=require('./routes/login.js');
-const shopRoutes=require('./routes/message.js');
-
+const adminRoutes=require('./routes/admin.js');
+const shopRoutes=require('./routes/shop.js');
+const path=require('path');
 app.use(adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
-	
-app.listen(2000);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,17 @@
 const express=require('express');
+const path=require('path');
 
 const router=express.Router();
-
-
-
     router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><br><input type="text" name="size"><br><button type="submit">add</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','admin.html'));
     });
     
-
     router.use('/add-product',(req,res,next)=>{
-    
+
     console.log('name of product is '+req.body.title);
-    console.log('size of product is '+req.body.size);
-    res.redirect('/shop');
-    });
     
+    res.redirect('/');
+    });
+
 
     module.exports=router;
